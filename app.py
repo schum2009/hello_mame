83345de940a4eef4c9ab2df96863b9f7774be1e8
Николай Кравченко <schum2009@gmail.com>
2022-01-17 13:54:50 +0200

    1

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', method=['GET', 'POST'])
+def hello():
+    return render_template('hello.html')
+
+
+@app.route('/hello_name', method=['POST'])
+def hello_name():
+    name = request.form.get('name')
+    return render_template('hello_name.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run()
